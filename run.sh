96f2d4318780206f44453372db81a90f19eb524b
smj <s.m.javadi001@gmail.com>
2022-09-24 15:13:08 +0330

    change - change run.sh and add final output and add minio

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ rm .env
 cp env.example .env
 sed -i "s/6547/$PORT/g" .env
 docker-compose up -d
+echo "Your application is ready to use:\n\ndownload:\tHOST:$PORT/FILE\n\nmonitoring:\tHOST:9113\n\nminio:\tHOST:$PORT/minio\n\tminio acccess key:\tPFTTDKXLCSZLGEYE\n\tminio secret key:\t7jC7YD22DaS8L3AQKW7yCDPl0UvXlXXj"
